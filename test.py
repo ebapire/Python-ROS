d97e4d9b53f066bb36be063779478352458c9880
ebapire <evagarciadomingo@gmail.com>
2017-05-09 18:37:08 +0200

    message

--- test.py
+++ test.py
@@ -11,6 +11,13 @@ class RosTest(unittest.TestCase):
         self.assertEqual(expected, got.y)
         self.assertEqual(expected, got.z)
 
+    def test_newmessage(self):
+        data = [1,1,1]
+        expected_coor = 1
+        expected_type = 1
+        got = Message(1, data)
+        self.assertEqual(expected_type, got[0])
+
 
 if __name__ == '__main__':
     unittest.main()
